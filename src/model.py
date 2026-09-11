85d0c75de4fa79da44d885fcfb0897b7c5c06f5a
Be Georgino Laindraika <belaindr@c1r13s14.42antananarivo.mg>
2026-09-11 21:47:04 +0300

    src/model

--- src/model.py
+++ src/model.py
@@ -1,24 +1,24 @@
 """Contains the model for prompt, return types and parametres."""
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Literal
 
 
 class Parameter(BaseModel):
     """Definiton of function parameter."""
-
+    model_config = ConfigDict(extra="forbid")
     type: Literal["number", "string", "boolean"]
 
 
 class ReturnType(BaseModel):
     """Definition of a function return type."""
-
+    model_config = ConfigDict(extra="forbid")
     type: Literal["number", "string", "boolean"]
 
 
 class FunctionDefinition(BaseModel):
     """A single callable function like in the functon_definitions."""
-
+    model_config = ConfigDict(extra="forbid")
     name: str
     description: str
     parameters: dict[str, Parameter]
@@ -27,13 +27,14 @@ class FunctionDefinition(BaseModel):
 
 class PromptEntry(BaseModel):
     """A single entry from function-calling_test."""
+    model_config = ConfigDict(extra="forbid")
 
     prompt: str
 
 
 class FunctionCallresult(BaseModel):
     """A single entry in the output file."""
-
+    model_config = ConfigDict(extra="forbid")
     prompt: str
     name: str
     parameters: dict[str, object]
