096c8c7f42b9dde44e304b04f8f99a693b3151ca
Be Georgino Laindraika <belaindr@c1r13s12.42antananarivo.mg>
2026-09-14 15:17:35 +0300

    fixing src

--- src/model.py
+++ src/model.py
@@ -15,6 +15,18 @@ class Parameter(BaseModel):
 
     type: Literal["number", "string", "boolean", "integer"]
 
+    @model_validator(mode="before")
+    @classmethod
+    def fix_parameter_type(cls, data: object) -> object:
+        """Force an invalid parameter type to a valid type."""
+        if not isinstance(data, dict):
+            return data
+
+        if data.get("type") not in VALID_TYPES:
+            data["type"] = "string"
+
+        return data
+
 
 class ReturnType(BaseModel):
     """Definition of a function return type."""
